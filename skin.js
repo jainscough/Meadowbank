b0c745ca170ab38e39eff8ad19f7acd47ab9f433
jo <jainscough@lumination.com.au>
2022-04-02 00:07:53 +1030

    change notes

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.14/18105
 // Filename: Meadowbank Skin Try this.ggsk
-// Generated 2022-04-01T17:17:15
+// Generated 2022-04-02T00:04:31
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, false);
